2f68de490d1a78122b7f77b0a568c848456641c7
BrenoUtimura <94381719+BrenoUtimura@users.noreply.github.com>
2024-03-05 01:09:57 -0300

    Add files via upload

---
+++ Spotfy/script.js
@@ -0,0 +1,37 @@
+const resultArtist = document.getElementById("result-artist");
+const playlistContainer = document.getElementById("result-playlist");
+const searchInput = document.getElementById("search-input");
+
+function requestApi(searchTerm) {
+   fetch(`http://localhost:3000/artists?name_like=${searchTerm}`)
+    .then((response) => response.json())
+    .then((results) => console.log(results));
+}
+
+function displayResults(results) {
+  resultPlaylist.classList.add('hidden');
+  const artistImage = document.getElementById("artist-img");
+  const artistName = document.getElementById("artist-name");
+
+  results.forEach((element) => {
+    artistImage.src = element.urlImg;
+    artistName.innerText = element.name;
+  });
+  resultArtist.classList.remove("hidden");
+}
+
+function hidePlaylists() {
+  playlistContainer.classList.add("hidden");
+}
+
+searchInput.addEventListener("input", function () {
+  const searchTerm = searchInput.value.toLowerCase();
+  if (searchTerm === "") {
+    resultArtist.classList.add("hidden");
+    playlistContainer.classList.remove("hidden");
+    return;
+  }
+
+  requestApi(searchTerm);
+});
+
